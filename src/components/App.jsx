b97af7e0629599adc5bb983235f04179214621c3
benunderdahl <benjamin.underdahl@proton.me>
2025-10-01 19:40:42 -0500

    added delete note function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
 import notes from "../notes"
-import "../../public/styles.css"
+import CreateArea from "./CreateArea";
 
 
 
 function App() {
+    const [n, setNotes] = useState(notes)
+    function deleteNote(id) {
+        setNotes((prev) =>  prev.filter((note, index) => index !== id))
+    }
     return <div>
         <Header />
-        {notes.map(item => <Note 
+        <CreateArea />
+        {n.map((item, index) => <Note 
             key={item.id}
+            id={index}
             title={item.title}
             content={item.content}
+            onDelete={deleteNote}
             />
         )}
         <Footer />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import "../../public/styles.css"
+
 
 
 function Note(props) {
-    return <div className="note">
-        <h1>
-            {props.title}
-        </h1>
-        <p>
-            {props.content}
-        </p>
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={() => {
+        props.onDelete(props.id)
+      }}>DELETE</button>
     </div>
+  );
 }
 
-export default Note
+export default Note;
